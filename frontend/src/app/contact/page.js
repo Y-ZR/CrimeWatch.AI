70b6325e62ca9a53a59d0f9790ac0bc2b798e361
Kalaris Ng <kalaris3255@gmail.com>
2024-04-27 18:43:43 -0700

    Add form in contact page

--- frontend/src/app/contact/page.js
+++ frontend/src/app/contact/page.js
@@ -1,10 +1,39 @@
 import React from "react";
 import { Header } from "@/components/ui/Header";
+import { Form } from "@/components/ui/Form";
 
 const ContactPage = () => {
   return (
     <div>
       <Header />
+      <section className="min-h-screen w-full flex items-center">
+        <div className="container px-4 md:px-6">
+          <div className="grid gap-6 grid-cols-[1fr_1fr]">
+            <div className="flex flex-col justify-center space-y-4">
+              <div className="space-y-2">
+                <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                  Contact us
+                </h1>
+                <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-m/relaxed dark:text-gray-400">
+                  Need to get in touch with us? Fill in the form with your
+                  enquiry and we will get back to you shortly.
+                </p>
+              </div>
+            </div>
+            <div className="mx-15">
+              <Form />
+            </div>
+
+            {/* <img
+              alt="Hero"
+              className="mx-auto aspect-video overflow-hidden rounded-xl object-cover object-center sm:w-full lg:order-last"
+              height="310"
+              src="/stock.jpeg"
+              width="550"
+            /> */}
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
---
+++ frontend/src/components/ui/Form.jsx
@@ -0,0 +1,57 @@
+"use client";
+
+export const Form = () => {
+  return (
+    <div className="border-2 p-4 tracking-tighter sm:text-s shadow-md w-3/4 rounded-sm">
+      <form
+        method="POST"
+        action="https://www.formbackend.com/f/664decaabbf1c319"
+      >
+        <div>
+          <div>
+            <label>Name</label>
+          </div>
+          <div>
+            <input
+              type="text"
+              name="name"
+              className="border-2 w-3/4 rounded-md p-1 bg-stone-100"
+            />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <div>
+            <label>Email</label>
+          </div>
+          <div>
+            <input
+              type="text"
+              name="email"
+              className="border-2 w-3/4 rounded-md p-1 bg-stone-100"
+            />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <div>
+            <label>What can we help you with?</label>
+          </div>
+          <div>
+            <textarea
+              name="message"
+              className="border-2 w-3/4 rounded-md p-1 bg-stone-100"
+            ></textarea>
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="bg-neutral-600 text-white px-5 rounded-sm"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
